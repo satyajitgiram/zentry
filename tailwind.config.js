bc4f710444925995d65f04683d4c3bef94460179
Satyajit Giram <satyajitgiram2000@gmail.com>
2024-12-05 09:45:48 +0530

    Add About component with GSAP animations and update Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ export default {
       fontFamily: {
         zentry: ['zentry', 'sans-serif'],
         general:['general','sans-serif'],
-        'cirucular-web':['circular-web','sans-serif'],
+        'circular-web':['circular-web','sans-serif'],
         'robert-medium':['robert-medium','sans-serif'],
         'robert-regular':['robert-regular','sans-serif'],
       },
